5607808c7abb72f98f5a16c9bfb062c665411be8
sg-abel <s_abel@ymail.com>
2023-04-18 21:27:26 -0600

    FizzBuzz

---
+++ FizzBuzz/FizzBuzz.js
@@ -0,0 +1,10 @@
+function fizzBuzz(){
+    for (let i = 1; i <= 100; i ++) {
+        if(i % 15 === 0) console.log('fizzBuzz')
+        else if (i % 3 === 0) console.log('fizz')
+        else if (i % 5 === 0) console.log('buzz')
+        else console.log(i)
+    }
+}
+
+fizzBuzz();
